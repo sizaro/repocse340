ccb2f0dddb2ab9579fef0d2b80345fc9a4a5ce90
sizaro <spartansiza@gmail.com>
2024-10-07 02:33:38 -0700

    pool..

--- database/index.js
+++ database/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV == "development") {
 }
 
 else {
-    pool = new pool({
+    pool = new Pool({
         connectionString: process.env.DATABASE_URL,
     })
     module.exports = pool
